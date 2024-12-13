19363ae308106ed5c8744180e70981e325c10e20
focos <Markbrendle5@gmail.com>
2024-12-13 16:54:56 -0500

    making a chose your race program

--- Coding/Chosing_your_Race.java
+++ Coding/Chosing_your_Race.java
@@ -11,6 +11,7 @@ public class Chosing_your_Race {
         System.out.println("2. Elf");
         System.out.println("3. Dwarf");
         System.out.println("4. Halfling");
+        System.out.println("5. Dragonborn");
         int species = hello.nextInt();
         if (species == 2) {
             System.out.println("You chose elf your stats are 1 Str, 1 Dex, 1 Con, 1 Wis, 1 Int, 1 Cha");
@@ -20,9 +21,11 @@ public class Chosing_your_Race {
             System.out.println("You chose Dwarf your stats are 1 Str, 1 Dex, 1 Con, 1 Wis, 1 Int, 1 Cha");
         }if (species == 4) {
             System.out.println("You chose Halfling your stats are 1 Str, 1 Dex, 1 Con, 1 Wis, 1 Int, 1 Cha");
-        }if (species > 4) {
+        }if (species > 5) {
             System.out.println("you are a ant");
             System.out.println("its stats are 0 Str, 1 Dex, 0 Con, 0 Wis, 0 Int, 0 Cha");
+        }if (species == 5) {
+            System.out.println("You chose Dragonborn your stats are 10 Str, 10 Dex, 10 Con, 10 Wis, 10 int, 10 Cha");
         }
     }
 
