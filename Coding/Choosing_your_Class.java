c9e6bbb065e2c2941edcf3334603474dd0d14163
aidan7039 <coolgamer17856@gmail.com>
2024-12-13 17:41:21 -0500

    minor changes to the barbarian class

--- Coding/Choosing_your_Class.java
+++ Coding/Choosing_your_Class.java
@@ -20,7 +20,7 @@ public class Choosing_your_Class {
 
         if (Class == 1) {
             System.out.println("You chose Barbarian, you fight with rage and brute force to defeat your enemies.");
-            System.out.println(" Your hit die is 1d12, you are proficient with light armor, medium armor, shields, simple weapons, and martial weapons");
+            System.out.println("Your hit die is 1d12, you are proficient with light armor, medium armor, shields, simple weapons, and martial weapons");
             System.out.println("Choose some starting equipment");
             System.out.println("1. A greataxe");
             System.out.println("2. Any martial weapon");
@@ -47,7 +47,7 @@ public class Choosing_your_Class {
             }
         }
 
-
+    scanner.close();
 
     }
 }
