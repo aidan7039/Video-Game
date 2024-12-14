6a45625976ded7b9039aabc662821682d721c14a
Noah <pokemonmanboy@yahoo.com>
2024-12-13 19:15:23 -0500

    Wizard starting block

--- Coding/Choosing_your_Class.java
+++ Coding/Choosing_your_Class.java
@@ -117,7 +117,10 @@ public class Choosing_your_Class {
                 }
 
             }
-
+        if (Class == 7) {
+            System.out.println("You chose Wizard, You use intelligence and wisdom instead of strength in battle")
+            System.out.println("")
+        }
 
         }
 
