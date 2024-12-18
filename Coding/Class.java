b0b9fde3b01d47f9cc846e88762c67f1e21fbaa3
focos <Markbrendle5@gmail.com>
2024-12-18 09:37:49 -0500

    creating player

---
+++ Coding/Class.java
@@ -0,0 +1,16 @@
+package Coding;
+
+public class Class {
+
+    public static void main(String[] args) {
+        int str, dex, con, Int, wis, cha, a;
+        str = 10;
+        dex = 10;
+        con = 10;
+        Int = 10;
+        wis = 10;
+        cha = 10;
+        a = str + dex + con + Int + wis + cha;
+        System.out.println(str + 1, dex + 1, con, Int, wis, cha);
+    }
+}
---
+++ Coding/Creatures/Player.java
@@ -0,0 +1,7 @@
+package Coding.Creatures;
+
+public class Player {
+    public static void main(String[] args) {
+
+    }
+}
