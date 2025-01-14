c976de079499365829279c5cc357ceb185428756
focos <Markbrendle5@gmail.com>
2025-01-14 11:20:55 -0500

    fixing class class

--- Coding/Creatures/Player.java
+++ Coding/Creatures/Player.java
@@ -1,7 +1,10 @@
 package Coding.Creatures;
-
+import Coding.Creatures.Class;
 public class Player {
+
+    static Class basicStats = new Class();
     public static void main(String[] args) {
+        System.out.println(basicStats.a);
 
     }
 }
