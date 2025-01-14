002e407f32d907d1b64fb3a866c8d5c3c9a70a22
focos <Markbrendle5@gmail.com>
2025-01-14 12:20:22 -0500

    fixing class class

--- Coding/Creatures/Player.java
+++ Coding/Creatures/Player.java
@@ -5,6 +5,13 @@ public class Player {
     static Class basicStats = new Class();
     public static void main(String[] args) {
         System.out.println(basicStats.a);
-
+        System.out.println(basicStats.Str);
+        System.out.println(basicStats.Dex);
+        System.out.println(basicStats.Con);
+        System.out.println(basicStats.Int);
+        System.out.println(basicStats.Wis);
+        System.out.println(basicStats.Cha);
+        int i = basicStats.Str + 2;
+        System.out.println(i);
     }
 }
