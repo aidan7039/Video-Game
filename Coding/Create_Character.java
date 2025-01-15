98639df4e4ff264a5da189d08b3a30b4f57b5238
focos <Markbrendle5@gmail.com>
2025-01-15 12:08:10 -0500

    moved everything out of the creatures folder

--- Coding/Create_Character.java
+++ Coding/Create_Character.java
@@ -1,7 +1,8 @@
 package Coding;
 
 public class Create_Character {
+    static Class basicStats = new Class();
     public static void main(String[] args) {
-
+        System.out.println(basicStats.a);
     }
 }
