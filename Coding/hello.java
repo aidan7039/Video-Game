cceeea7d0225454489a1a846629d90e68a81b782
focos <Markbrendle5@gmail.com>
2025-01-19 11:09:07 -0500

    edits

--- Coding/hello.java
+++ Coding/hello.java
@@ -4,6 +4,7 @@ public class hello {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
+        System.out.println(a);
 
     }
 }
