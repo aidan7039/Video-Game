2b616eedea686bd1976b6bc9c60217847c15aef5
focos <Markbrendle5@gmail.com>
2025-01-24 10:01:19 -0500

    hello

--- Coding/Create_Character.java
+++ Coding/Create_Character.java
@@ -635,6 +635,7 @@ public class Create_Character {
         System.out.println("");
         System.out.println("hello " + name + " ever since the great dragon came and destroyed the great Kingdom of Paladindra ever since the world of fadfeirld has been in great turmoil.");
         System.out.println("You are in the world of Fadfeirld (Noah please change this later) the realm is in chaos and turmoil after the great dragon named dog came and started terrorizing the  land.");
+        System.out.println("please state which direction you want to go in");
         String hi = hello.nextLine();
         System.out.println(hi);
     }
