dafb2252a056454a646d977992ac9bc56e06bf60
aidan7039 <coolgamer17856@gmail.com>
2025-01-30 09:47:15 -0500

    I got the player to be able to die (i think)

--- Coding/Testing_Something.java
+++ Coding/Testing_Something.java
@@ -14,7 +14,7 @@ public class Testing_Something {
         //System.out.println(basicStats.a);
         int health = 3 + dice.d10;
         int AC = 13;
-        int t1 = dice.d61 + dice.d62+ dice.d63;
+        int t1 = dice.d61 + dice.d62 + dice.d63;
         int t2 = dice.d64 + dice.d65 + dice.d66;
         int t3 = dice.d67 + dice.d68 + dice.d69;
         int t4 = dice.d610 + dice.d611 + dice.d612;
@@ -35,17 +35,18 @@ public class Testing_Something {
         //THE BELOW CODE IS AN EXAMPLE OF WHAT A COMBAT ENCOUNTER MIGHT LOOK LIKE
         dice.d6gbscimitar = dice.d6gbscimitar + 2;
 
-        System.out.println("Your maximum health is " + health + ".");
-        System.out.println("You've been spotted by a goblin they're about to attack you!");
-        int goblinrl = dice.d20gb;
-        if (goblinrl >= AC) {
-            System.out.println("You've been hit! You take " + (dice.d6gbscimitar) + " damage.");
-            System.out.println("Your health is now " + (health - dice.d6gbscimitar + "."));
-        } else {
-            System.out.println("The goblin missed it is your turn to attack.");
-        }
-
-
+            System.out.println("Your maximum health is " + health + ".");
+            System.out.println("You've been spotted by a goblin they're about to attack you!");
+            int goblinrl = dice.d20gb;
+            if (goblinrl >= AC) {
+                System.out.println("You've been hit! You take " + (dice.d6gbscimitar) + " damage.");
+                System.out.println("Your health is now " + (health - dice.d6gbscimitar + "."));
+                if (health <= 0){
+                    System.out.println("You are dead. May the gods bless you in the afterlife");
+                }
+            } else {
+                System.out.println("The goblin missed it is your turn to attack.");
+            }
 
     }
 }
