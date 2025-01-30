e36f98a1a3e87dcaff94e1ce8af2570c05fa1a67
focos <Markbrendle5@gmail.com>
2025-01-30 11:13:17 -0500

    finished the asking for directions

--- Coding/Where_do_you_go.java
+++ Coding/Where_do_you_go.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Where_do_you_go {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String strait = "strait";
+        String strait = "straight";
         String right = "right";
         String left = "left";
         String look_around = "look around";
@@ -18,7 +18,19 @@ public class Where_do_you_go {
                 System.out.println("your are heading towards a cliff");
                 System.out.println("do you want to keep going?");
             }
-        scanner.close();
+            if (userAnswer.equalsIgnoreCase(left)) {
+                System.out.println("you are in a river");
+                System.out.println("do you want to try to swim");
+            }
+            if (userAnswer.equalsIgnoreCase(look_around)) {
+                System.out.println("to your right there you see that you are at the top of a cliff");
+                System.out.println("to your left you see that you are next to a giant river");
+                System.out.println("strait ahead of you there is a town");
+                System.out.println("");
+                System.out.println("");
+            } else {
+                System.out.println("try again");
+            }
+        }
     }
-}
 }
