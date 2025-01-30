def183d8beb77377c8229a17d98683a16f394f9c
focos <Markbrendle5@gmail.com>
2025-01-30 15:38:33 -0500

    fixed capitalization

--- Coding/Where_do_you_go.java
+++ Coding/Where_do_you_go.java
@@ -4,32 +4,32 @@ public class Where_do_you_go {
 Create_Character cc = new Create_Character();
     public static void Run4() {
     //public static void main(String[] args) {
-        String strait = "straight";
-        String right = "right";
-        String left = "left";
+        String strait = "North";
+        String right = "East";
+        String left = "West";
         String look_around = "look around";
         String userAnswer;
-        System.out.println("what direction do you want to go in");
+        System.out.println("What direction do you want to go in");
         Scanner can = new Scanner(System.in);
         while(true) {
             userAnswer = can.nextLine();
             if (userAnswer.equalsIgnoreCase(strait)) {
                 MainController.handleAction("CombatTest");
-                System.out.println("you enter the town");
+                System.out.println("You enter the town");
             }
             if (userAnswer.equalsIgnoreCase(right)) {
-                System.out.println("your are heading towards a cliff");
-                System.out.println("do you want to keep going?");
+                System.out.println("Your are heading towards a cliff");
+                System.out.println("Do you want to keep going?");
                 MainController.handleAction("CombatTest");
             }
             if (userAnswer.equalsIgnoreCase(left)) {
-                System.out.println("you are in a river");
-                System.out.println("do you want to try to swim");
+                System.out.println("You are in a river");
+                System.out.println("Do you want to try to swim");
             }
             if (userAnswer.equalsIgnoreCase(look_around)) {
-                System.out.println("to your right there you see that you are at the top of a cliff");
-                System.out.println("to your left you see that you are next to a giant river");
-                System.out.println("strait ahead of you there is a town");
+                System.out.println("To your right there you see that you are at the top of a cliff");
+                System.out.println("To your left you see that you are next to a giant river");
+                System.out.println("Strait ahead of you there is a town");
                 System.out.println("");
                 System.out.println("");
             }
