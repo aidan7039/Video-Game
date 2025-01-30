64636f74fb438b2c56e6247eca4185f6b49be2b1
focos <Markbrendle5@gmail.com>
2025-01-30 13:59:24 -0500

    working on everything running in the same program

--- Coding/RUN.java
+++ Coding/RUN.java
@@ -4,7 +4,6 @@ public class RUN {
     public static void main(String[] args) {
 
         MainController.handleAction("Create_Character");
-
         MainController.handleAction("Where_do_you_go");
     }
 
