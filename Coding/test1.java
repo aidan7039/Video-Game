2396cba409c6fbc78610ac3a13733f3841f08be0
focos <Markbrendle5@gmail.com>
2025-03-05 09:22:38 -0500

    Will this work

--- Coding/test1.java
+++ Coding/test1.java
@@ -3,7 +3,7 @@ package Coding;
 public class test1 {
      String story = "This is the story of the land of Gimberdale";
      public static void main(String[] args) {
-          System.out.println("Hello World");
+          System.out.println("hello");
 
      }
 }
