98479276cc97eed588ce9f265d03dab7c351e5f2
aidan7039 <coolgamer17856@gmail.com>
2025-03-25 13:50:57 -0400

    I finally figured out how to make objects of a class that I've made!!! Methods to the Character class have been added.

---
+++ Coding/Map.java
@@ -0,0 +1,28 @@
+package Coding;
+
+public class Map {
+
+    public static void main(String[] args) {
+        //each line is 40 characters long
+        String mln1 = ("________________________________________");
+        String mln2 = ("|                                      |");
+        String mln3 = ("|                                      |");
+        String mln4 = ("|                                      |");
+        String mln5 = ("|                                      |");
+        String mln6 = ("|                                      |");
+        String mln7 = ("|                                      |");
+        String mln8 = ("|                                      |");
+        String mln9 = ("|                                      |");
+        String mln10 = ("________________________________________");
+        System.out.println(mln1);
+        System.out.println(mln2);
+        System.out.println(mln3);
+        System.out.println(mln4);
+        System.out.println(mln5);
+        System.out.println(mln6);
+        System.out.println(mln7);
+        System.out.println(mln8);
+        System.out.println(mln9);
+        System.out.println(mln10);
+    }
+}
